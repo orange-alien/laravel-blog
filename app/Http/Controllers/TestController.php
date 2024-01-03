8c121c6d551b3b3633b7446f9d27c7be1d3980c0
orange-alien <6963921+orange-alien@users.noreply.github.com>
2024-01-03 07:21:44 +0000

    サブクエリの例を追加

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -22,11 +22,18 @@ class TestController extends Controller
         dump($query->count());
 
         // カウント(期待通りの結果になる)
+        // SQL : select * from (select `users`.`id`, COUNT(*) as count from `users` inner join `posts` on `users`.`id` = `posts`.`user_id` group by `users`.`id`) as `sub`
+        // 出力 : 7
+        $query2 = DB::query()->fromSub($query, 'sub');
+        dump($query2->toRawSql());
+        dump($query2->count());
+
+        // カウント(期待通りの結果になる)
         // SQL : select distinct `user_id` from `posts`;
         // 出力 : 7
-        $query2 = DB::table('posts')->select('user_id')->distinct('user_id');
-        dump($query2->toRawSql());
-        dd($query2->count());
+        $query3 = DB::table('posts')->select('user_id')->distinct('user_id');
+        dump($query3->toRawSql());
+        dd($query3->count());
 
         return null;
     }
