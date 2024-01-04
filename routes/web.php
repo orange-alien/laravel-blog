4e52f481421dc260b70a2aafc555eb2fa81ca740
orange-alien <6963921+orange-alien@users.noreply.github.com>
2024-01-04 06:40:21 +0000

    ModelのwasRecentlyCreatedプロパパティの検証

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('/test')->group(function() {
+    // 動作検証を容易にするために GET で登録や更新を行っているが、
+    // 本来は POST や PUT を用いるべきである。
+    Route::get('/new',    [TestController::class, 'new']);
+    Route::get('/first',  [TestController::class, 'first']);
+    Route::get('/create', [TestController::class, 'create']);
+    Route::get('/save',   [TestController::class, 'save']);
+    Route::get('/save2',  [TestController::class, 'save2']);
 });
