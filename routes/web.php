1cd607707a1f7dba31e5c4190416ad64c4e13722
orange-alien <6963921+orange-alien@users.noreply.github.com>
2024-01-06 12:49:26 +0000

    テスト用のコントローラとroute

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('/test')->group(function() {
+    Route::get('/index' , [TestController::class, 'index']);
+    Route::get('/post'  , [TestController::class, 'post']);
+    Route::get('/put'   , [TestController::class, 'put']);
+    Route::get('/delete', [TestController::class, 'delete']);
+    Route::get('/json'  , [TestController::class, 'json']);
+    Route::get('/error/{code}', [TestController::class, 'error']);
 });
