cb2c7ef2bbacd83d286fb3d7c86606253c1cec33
orange-alien <6963921+orange-alien@users.noreply.github.com>
2024-01-06 03:09:36 +0000

    APIログ用のログチャンネルを作成

--- config/logging.php
+++ config/logging.php
@@ -82,6 +82,14 @@ return [
             'replace_placeholders' => true,
         ],
 
+        'api_log' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/api/api.log'),
+            'level' => env('API_LOG_LEVEL', 'debug'),
+            'days' => 14,
+            'replace_placeholders' => true,
+        ],
+
         'papertrail' => [
             'driver' => 'monolog',
             'level' => env('LOG_LEVEL', 'debug'),
