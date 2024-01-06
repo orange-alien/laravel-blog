2cf4095412606d32d61c355d68b653ee760f96e4
orange-alien <6963921+orange-alien@users.noreply.github.com>
2024-01-06 03:09:59 +0000

    リクエスト送信時のログ書き込み処理

--- app/Listeners/LogRequestSending.php
+++ app/Listeners/LogRequestSending.php
@@ -4,7 +4,9 @@ namespace App\Listeners;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Http\Client\Events\RequestSending;
+use Illuminate\Http\Client\Request;
 use Illuminate\Queue\InteractsWithQueue;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class LogRequestSending
 {
@@ -21,6 +23,29 @@ class LogRequestSending
      */
     public function handle(RequestSending $event): void
     {
-        //
+        $request = $event->request;
+        $requestSendingLogData['request'] = $this->requestSendingLogData($request);
+        $this->writeLog($requestSendingLogData);
+    }
+
+    // リクエスト送信時の内容
+    protected function requestSendingLogData(Request $request) : array
+    {
+        return [
+            'event'   => 'request.sending',
+            'method'  => $request->method(),
+            'url'     => $request->url(),
+            'headers' => $request->headers(),
+            'body'    => json_decode($request->body(), true),
+            'query'   => $request->method() === SymfonyRequest::METHOD_GET
+                            ? $request->data()
+                            : null,
+        ];
+    }
+
+    // ログの書き込み
+    protected function writeLog(array $logData, string $as = 'info'): void
+    {
+        logs()->channel('api_log')->{$as}( json_encode($logData, JSON_UNESCAPED_UNICODE) );
     }
 }
