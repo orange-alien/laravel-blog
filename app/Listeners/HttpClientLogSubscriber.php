c3f49ae7396f6dc3673d7287178fdc4fd06dc8b5
orange-alien <6963921+orange-alien@users.noreply.github.com>
2024-01-07 04:42:39 +0000

    HTTPクライアントのリクエスト・レスポンスのログ取得

--- app/Listeners/HttpClientLogSubscriber.php
+++ app/Listeners/HttpClientLogSubscriber.php
@@ -6,16 +6,31 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Http\Client\Events\RequestSending;
 use Illuminate\Http\Client\Events\ResponseReceived;
+use Illuminate\Http\Client\Request;
+use Illuminate\Http\Client\Response;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class HttpClientLogSubscriber
 {
+    // リクエストとレスポンスを紐付けるためのUUID
+    protected string $uuid = '';
+
+    // リクエスト送信時のログを出力するかどうか
+    protected bool $enableLogRequestSending   = false;
+    // リクエスト受信時のログを出力するかどうか
+    protected bool $enableLogResponseReceived = false;
+
     /**
      * Create the event listener.
      */
     public function __construct()
     {
-        //
+        $this->uuid = Str::uuid();
+
+        $this->enableLogRequestSending   = config('http_client.enable_log_request_sending');
+        $this->enableLogResponseReceived = config('http_client.enable_log_response_received');
     }
 
     /**
@@ -40,7 +55,20 @@ class HttpClientLogSubscriber
      */
     public function logRequestSending(RequestSending $event) : void
     {
-        \Log::debug(__METHOD__);
+        if(!$this->enableLogRequestSending) {
+            return;
+        }
+
+        // ログの内容
+        $request = $event->request;
+        $data = [
+            'uuid'    => $this->uuid,
+            'event'   => RequestSending::class,
+            'request' => $this->requestLog($request),
+        ];
+
+        // ログ出力
+        $this->writeLog($data);
     }
 
     /**
@@ -51,9 +79,68 @@ class HttpClientLogSubscriber
      */
     public function logResponseReceived(ResponseReceived $event) : void
     {
-        \Log::debug(__METHOD__);
+        if(!$this->enableLogResponseReceived) {
+            return;
+        }
+
+        // ログの内容
+        $request  = $event->request;
+        $response = $event->response;
+        $data = [
+            'uuid'     => $this->uuid,
+            'event'    => ResponseReceived::class,
+            'request'  => $this->requestLog($request),
+            'response' => $this->responseLog($response),
+        ];
+
+        // ログ出力
+        $response->successful()
+            ? $this->writeLog($data)
+            : $this->writeLog($data, as:'error');
     }
 
+    /**
+     * リクエスト送信時のログ
+     *
+     * @param  Request $request
+     * @return array
+     */
+    protected function requestLog(Request $request) : array
+    {
+        return [
+            'method'  => $request->method(),
+            'url'     => $request->url(),
+            'headers' => $request->headers(),
+            'body'    => json_decode($request->body(), true),
+            'query'   => $request->method() === SymfonyRequest::METHOD_GET
+                            ? $request->data()
+                            : null,
+        ];
+    }
 
+    /**
+     * レスポンス受信時のログ
+     *
+     * @param  Response $response
+     * @return array
+     */
+    protected function responseLog(Response $response) : array
+    {
+        return [
+            'status' => $response->status(),
+            'body'   => json_decode($response->body(), true),
+        ];
+    }
 
+    /**
+     * ログ出力
+     *
+     * @param  array $logData
+     * @param  string $as
+     * @return void
+     */
+    protected function writeLog(array $logData, string $as = 'info'): void
+    {
+        logs()->channel('http_client')->{$as}( json_encode($logData, JSON_UNESCAPED_UNICODE) );
+    }
 }
